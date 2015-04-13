45dd1521b12a4fdd0ac28bcdc4b3efe195c42ef9
= <avellaneda.gabriel@gmail.com>
2015-04-13 15:58:41 +0000

    wget with quiet mode and noclober

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,7 @@ sudo apt-get install git
 #sudo update-rc.d nginx defaults
 
 # Install Grafana from the deb package.
-sudo wget https://grafanarel.s3.amazonaws.com/builds/grafana_2.0.0-beta3_amd64.deb
+sudo wget -q -nc https://grafanarel.s3.amazonaws.com/builds/grafana_2.0.0-beta3_amd64.deb
 sudo apt-get install -y adduser libfontconfig
 sudo dpkg -i grafana_2.0.0-beta3_amd64.deb
 
