53424db8fb9b1f5b36044b2a6483b74c6c85232d
= <avellaneda.gabriel@gmail.com>
2015-04-14 13:18:13 +0000

    Added Hbase and OpenTSDB provisioning

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,7 +12,24 @@ sudo wget -q -nc https://grafanarel.s3.amazonaws.com/builds/grafana_2.0.0-beta3_
 sudo apt-get install -y adduser libfontconfig
 sudo dpkg -i grafana_2.0.0-beta3_amd64.deb
 
+# Install Grafana customizations from repository
+
 sudo update-rc.d grafana-server defaults 95 10
+
+# Install the Java JDK
+sudo apt-get install default-jdk
+
+# Install and configure HBase
+if ! [ -d /vagrant/hbase ]; then
+  wget -q -nc http://mirror.nbtelecom.com.br/apache/hbase/stable/hbase-1.0.0-bin.tar.gz
+  tar -xvzf hbase-1.0.0-bin.tar.gz /vagrant/hbase
+  # TODO: Configure hbase
+fi
+
+# Install OpenTSDB
+wget -q -nc https://github.com/OpenTSDB/opentsdb/releases/download/v2.1.0RC2/opentsdb-2.1.0RC2_all.deb
+sudo dpkg -i opentsdb-2.1.0RC2_all.deb
+
 
 #if ! [ -d /vagrant/grafana ]; then
 #
